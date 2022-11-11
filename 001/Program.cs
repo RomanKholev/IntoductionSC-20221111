063dda98655b17e182add162b7c10958ae422522
Roman Kholev <RKholev@gmail.com>
2022-11-11 21:46:48 +0300

    Добавили вторую задачу

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,5 +1,8 @@
-﻿/*
-int a=0, b=1, c=2; // целый
+﻿using System.Transactions;
+using System.Security;
+using Microsoft.CSharp.RuntimeBinder;
+/*
+int a=0, b=1, c=2; // целый 32 бита
 float f=2.04f; // вещ 32 бита
 double d=3.14; //веществ 64 бита
 string s="Текст" //Строака
@@ -8,7 +11,8 @@ bool flag=true // true или falsh
 */
 // 1. С клавиатуры вводится целое число. Вывести квадрат числа 
 int a;
+System.Console.WriteLine("Введите число: ");
 string? s=Console.ReadLine();
 a=Convert.ToInt32(s);
 int b=a*a;
-System.Console.WriteLine(b);
+System.Console.WriteLine(Convert.ToString(a)+"^2="+b.ToString());
