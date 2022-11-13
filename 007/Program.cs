9ec8e7dc5a783c250c27346886980587671b8d72
Roman Kholev <RKholev@gmail.com>
2022-11-13 18:14:59 +0300

    Добавлено задание 7

---
+++ 007/Program.cs
@@ -0,0 +1,11 @@
+﻿//Выяснить является ли число чётным
+System.Console.Write("Введите число: ");
+double a=Convert.ToDouble(Console.ReadLine());
+if (a%2==0)
+{
+   System.Console.Write($"{a} - Четное число"); 
+}
+else
+{
+    System.Console.Write($"{a} - Нечетное число"); 
+}
