946633a519606b7b9d1a751b420c483c5006ec84
Roman Kholev <RKholev@gmail.com>
2022-11-13 16:07:50 +0300

    Добавлены задания 6, 9, 11, 13

--- 001/Program.cs
+++ 001/Program.cs
@@ -11,8 +11,11 @@ bool flag=true // true или falsh
 */
 // 1. С клавиатуры вводится целое число. Вывести квадрат числа 
 int a;
-System.Console.WriteLine("Введите число: ");
+a=10;
+System.Console.Write("Введите число:");
 string? s=Console.ReadLine();
 a=Convert.ToInt32(s);
 int b=a*a;
 System.Console.WriteLine(Convert.ToString(a)+"^2="+b.ToString());
+System.Console.WriteLine("{0}^2={1}",a,b);
+System.Console.WriteLine($"{a}^2={b}");
