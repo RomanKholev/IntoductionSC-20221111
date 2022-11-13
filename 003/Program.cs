a8f02601308d5b81b7dc22319b2e08f91ce4621f
Roman Kholev <RKholev@gmail.com>
2022-11-13 17:35:37 +0300

    Добавлено задание 3

---
+++ 003/Program.cs
@@ -0,0 +1,17 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+System.Console.Write("Введите число а:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите число с:");
+double c=Convert.ToDouble(Console.ReadLine());
+double max = a;
+if (max < b)
+{
+    max = b;
+} 
+if (max < c) 
+{
+    max = c;
+}
+System.Console.WriteLine($"{max} является иаксимальным");
