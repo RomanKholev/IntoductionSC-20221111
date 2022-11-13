02b6d267d35eaebd312ff31af554343a9f823757
Roman Kholev <RKholev@gmail.com>
2022-11-13 18:07:21 +0300

    Добавлено задание 5

--- 003/Program.cs
+++ 003/Program.cs
@@ -3,15 +3,9 @@ System.Console.Write("Введите число а:");
 double a=Convert.ToDouble(Console.ReadLine());
 System.Console.Write("Введите число b:");
 double b=Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите число с:");
-double c=Convert.ToDouble(Console.ReadLine());
 double max = a;
 if (max < b)
 {
     max = b;
-} 
-if (max < c) 
-{
-    max = c;
 }
 System.Console.WriteLine($"{max} является иаксимальным");
---
+++ 005/Program.cs
@@ -0,0 +1,17 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+System.Console.Write("Введите число а:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите число с:");
+double c=Convert.ToDouble(Console.ReadLine());
+double max = a;
+if (max < b)
+{
+    max = b;
+} 
+if (max < c) 
+{
+    max = c;
+}
+System.Console.WriteLine($"{max} является иаксимальным");
