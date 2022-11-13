35ba5eed8c1929bbb592a7e8c308aff0abddd855
Roman Kholev <RKholev@gmail.com>
2022-11-13 19:17:59 +0300

    Добавлено задание 15

---
+++ 015/Program.cs
@@ -0,0 +1,13 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+System.Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int b = (a%100%10);
+if (a<100)
+{
+    System.Console.Write("NO");
+}
+else
+{
+System.Console.Write($"Третья цыфра числа: {a} - {b}");
+}
+
