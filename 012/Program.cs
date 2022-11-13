b18e736a191f76d26bcd8120d8adabde7fd61acb
Roman Kholev <RKholev@gmail.com>
2022-11-13 18:53:05 +0300

    Добавлено задание 12

---
+++ 012/Program.cs
@@ -0,0 +1,11 @@
+﻿// С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+System.Console.Write("Введите число в диапозон от 10 до 99: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int num1 = (a/10);
+int num2 = (a%10);
+int max = num1;
+if (max < num2)
+{
+    max = num2;
+} 
+System.Console.WriteLine($"Наиборльшая цифра: {max}");
