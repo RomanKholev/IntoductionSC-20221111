384d4eb2a0d85785c47b852f000d0f3db1701b9e
Roman Kholev <RKholev@gmail.com>
2022-11-17 14:15:22 +0300

    Добавлено задание 21

---
+++ 021/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+System.Console.Write("Введите номер дня четверти: ");
+int i=Convert.ToInt32(Console.ReadLine());
+while (i>4 || i<0)
+{
+System.Console.Write("Число от 1 до 4 ");
+i=Convert.ToInt32(Console.ReadLine());
+}
+string[] quter = {"X > 0, Y > 0", "X > 0, Y < 0", "X < 0, Y < 0", "X > 0, Y < 0",};
+System.Console.WriteLine($"{i} четверть, координаты: {quter[i-1]}"); 
