2e761362c7848984ab32f076b5837db4127c78c3
Roman Kholev <RKholev@gmail.com>
2022-11-19 16:32:43 +0300

    Добавлено задание 25

---
+++ 025/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+System.Console.WriteLine("Введите N");
+int n=Convert.ToInt32(Console.ReadLine());
+double Сube(int a)
+{
+  return Math.Pow(a,3);
+}
+
+for(int i=1;i<n+1;i++)
+{
+    System.Console.WriteLine(Сube(i));
+    
+}
