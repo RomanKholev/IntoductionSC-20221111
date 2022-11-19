f22c5a4e05fb7ba1dd880be887ad98c02df05c56
Roman Kholev <RKholev@gmail.com>
2022-11-19 15:44:28 +0300

    Добавлены задания 3 семинара

--- BasicAlgoritms/002-Счетчик и Флаг/Program.cs
+++ BasicAlgoritms/002-Счетчик и Флаг/Program.cs
@@ -1,4 +1,5 @@
-﻿/*Напишите программу для решения следующей задачи. 
+﻿/*
+Напишите программу для решения следующей задачи. 
 На  контрольной  работе  по  алгебре  ученикам 9 класса  было  предложено 
 10 примеров.  Неудовлетворительная  оценка  выставляется,  если  правильно 
 решено  менее  половины  примеров.  Сколько  неудовлетворительных  оценок 
@@ -6,16 +7,18 @@
 все задачи, выведите YES, иначе – выведите NO. 
 Программа получает на вход количество учеников в классе N (1 ≤ N ≤ 30), 
 затем  для  каждого  ученика  вводится  количество  правильно  решённых 
-примеров. 
+примеров.
 */
+
+
 int n=Convert.ToInt32(Console.ReadLine());
-int k=0;
-bool flag=false;
-for (int i=0;i<n;i++)
+int k=0;//Счетчик
+bool flag=false;//флаг
+for(int i=0;i<n;i++)
 {
     int a=Convert.ToInt32(Console.ReadLine());
     if (a<5) k++;
     if (a==10) flag=true;
 }
 System.Console.WriteLine(k);
-if (flag) System.Console.WriteLine("Yas"); else System.Console.WriteLine("No");
+if (flag) System.Console.WriteLine("YES"); else System.Console.WriteLine("NO");
