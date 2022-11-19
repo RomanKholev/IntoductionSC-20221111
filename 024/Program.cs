102b6202c9fec29245726786273a66983db5cece
Roman Kholev <RKholev@gmail.com>
2022-11-19 16:11:42 +0300

    Добавлено задание 24

---
+++ 024/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+System.Console.WriteLine("Введите N");
+int n=Convert.ToInt32(Console.ReadLine());
+double Square (int a)
+{
+  return a=a*a;
+}
+
+for(int i=1;i<n+1;i++)
+{
+    System.Console.WriteLine($"{i}^2={Square(i)}");
+    
+}
