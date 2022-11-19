738cb3daedb5eb465fe748004c3a188e3888a132
Roman Kholev <RKholev@gmail.com>
2022-11-19 17:10:49 +0300

    Добавлено задание 27

---
+++ 027/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+System.Console.WriteLine("Введите A");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите B");
+int b=Convert.ToInt32(Console.ReadLine());
+
+double Degree(double a, int b)
+{
+    double result=a;
+for (int i=1;i<b;i++)
+{
+   result=result*a; 
+}
+return result;
+}
+System.Console.WriteLine($"{a}^{b}={Degree(a,b)}");
