3fd1bfb0b7e83d2837e4b5665ecd8b8c480d77b0
Roman Kholev <RKholev@gmail.com>
2022-11-19 17:29:19 +0300

    Добавлено задание 29

---
+++ 029/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе. Сделать подпрограмму.
+int SumOfNumbers (int a)
+{
+    int sum=0;
+    while (a>=1)
+    {
+     sum = sum+a%10;
+     a=a/10;
+    }
+    
+return sum;
+}
+System.Console.WriteLine("Введите A");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SumOfNumbers(a));
