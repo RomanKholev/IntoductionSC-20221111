87e1393c2aef01388c007c98873dce1d36a98667
Roman Kholev <RKholev@gmail.com>
2022-11-24 14:16:45 +0300

    Исправленоо задания 4 семинара

---
+++ BasicAlgoritms/005-switch/Program.cs
@@ -0,0 +1,33 @@
+﻿// С клавиатуры вводится число обозначающее день недли. Определить день недели.
+int day=2;
+//if (day==1) System.Console.WriteLine("Понедельник");
+//if (day==2) System.Console.WriteLine("Вторник");
+
+switch (day)
+{
+    case 1: 
+    System.Console.WriteLine("Понедельник");
+    break;
+case 2: 
+    System.Console.WriteLine("Вторник");
+    break;
+case 3: 
+    System.Console.WriteLine("Среда");
+    break;
+case 4: 
+    System.Console.WriteLine("Четверг");
+    break;
+case 5: 
+    System.Console.WriteLine("Пятница");
+    break;
+case 6: 
+    System.Console.WriteLine("Суббота");
+    break;
+case 7: 
+    System.Console.WriteLine("Воскресенье");
+    break;
+
+    default:
+    System.Console.WriteLine("Неапавильный ввод");
+    break;
+}
---
+++ BasicAlgoritms/007-Arrays(Массивы)/Program.cs
@@ -0,0 +1,15 @@
+﻿// Расчитать среднюю температуру за неделю
+//массивы - это объекты
+int N=7;
+int[] t;//не присвоенное (null)
+t=new int[N];//создаем массив, то есть в t помещаем адрес массива
+int[] t2=new int[N];
+double s=0;
+//заполнить массив
+for(int i=0;i<t.Length;i++)
+    t[i]=Convert.ToInt32(Console.ReadLine());
+//обработать массив
+for(int i=0;i<t.Length;i++)    
+    s=s+t[i];
+//вывод результата
+System.Console.WriteLine(s/t.Length);
