828ffc4bb9adc655659ffcf5bde3bba7fb11da30
Roman Kholev <RKholev@gmail.com>
2022-11-27 18:05:35 +0300

    Добавлены задания 5 семинар

---
+++ 051/Program.cs
@@ -0,0 +1,18 @@
+﻿// Перевод 10 в двоичную
+int n=123;
+string DecToBin(int n)
+{
+   string s="";
+wheil(n!=0)
+{
+    s=Convert.ToString(n%2);
+    n/=2;
+} 
+}
+string s="";
+wheil(n!=0)
+{
+    s=Convert.ToString(n%2);
+    n/=2;
+}
+System.Console,WriteLine(s);
