756de50bf786db4c30a79c9bda9ccbe7881bc76a
Roman Kholev <RKholev@gmail.com>
2022-11-28 14:25:01 +0300

    Добавлено задание 56

---
+++ 056/Program.cs
@@ -0,0 +1,31 @@
+﻿// 56 Написать программу копирования массива
+
+int[] RandomIntArray(int N,int min,int max)
+{
+    int[] a=new int[N];
+    Random random=new Random();
+    for(int i=0;i<a.Length;i++)
+        a[i]=random.Next(min,max);
+    return a;
+}
+
+void PrintArray(int[] a,int tab=5)
+{
+    string formatString="{0,"+tab+"}";
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write(formatString,a[i]);
+}
+
+int[] Copy (int [] a)
+{
+    int []  copy = new int[a.Length];
+    for(int i=0;i<a.Length;i++)
+        copy[i]=a[i];
+    return copy;
+}
+
+int [] a=RandomIntArray(10, 0, 100);
+int [] b=Copy(a);
+PrintArray(a);
+System.Console.WriteLine();
+PrintArray(b);
