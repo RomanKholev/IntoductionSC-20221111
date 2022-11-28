3fda71a74f6d3043c5d42bf22e88ae8058788bcd
Roman Kholev <RKholev@gmail.com>
2022-11-28 10:30:41 +0300

    Добавлено задание 55

---
+++ 055/Program.cs
@@ -0,0 +1,61 @@
+﻿// 55 Написать программу масштабирования фигуры
+
+double [] Coordinats (int size=3)
+{
+    if(size<3) 
+    {
+      System.Console.WriteLine("Введите координатыт минимум 3 точек:");
+      size=3;  
+    }
+System.Console.WriteLine("Введите координатыт точек:");
+int n=size*2;   
+double [] a=new double[n];
+  int count=0;
+ for(int i=0;i<n;i++)
+    {
+        System.Console.Write($"X{count}=");
+        a[i]=Convert.ToDouble(System.Console.ReadLine());  
+        i++;
+        System.Console.Write($"Y{count}=");
+        a[i]=Convert.ToDouble(System.Console.ReadLine());
+        count++;  
+    }
+        
+    
+return a;
+}
+
+double [] Scale (double [] a, double K=1)
+{
+    int size=a.Length;
+double [] b=new double[size];
+    b[0]=a[0];        
+    b[1]=a[1];
+    for(int i=2;i<a.Length;i++)
+    b[i]=a[i]*K;
+return b;
+}  
+
+
+
+void PrintPoint(double[] a)
+{
+    int count=0;
+        for(int i=0;i<a.Length;i++)
+        {
+        System.Console.Write($"X{count}={a[i],3}  ");
+        i++;
+        System.Console.Write($"Y{count}={a[i],3} | ");
+        count++;
+        }
+}
+
+System.Console.WriteLine("Введите количество точек фигуры: ");
+int N=Convert.ToInt32(System.Console.ReadLine()); 
+System.Console.WriteLine("Введите коэффициент масштабирования: ");
+double K=Convert.ToDouble(System.Console.ReadLine());   
+double [] a=Coordinats(N);
+System.Console.WriteLine("Координаты исхожной фигуры: ");
+PrintPoint(a);
+System.Console.WriteLine("Координаты фигуры после масштабирования: ");
+PrintPoint(Scale(a, 0.5));
