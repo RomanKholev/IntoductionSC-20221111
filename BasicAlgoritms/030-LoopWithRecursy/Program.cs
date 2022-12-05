d56140e227d87ed8a514c0dd3dd78054a0aff615
Roman Kholev <RKholev@gmail.com>
2022-12-05 15:19:14 +0300

    Добавлены задания 7 семинара

---
+++ BasicAlgoritms/030-LoopWithRecursy/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//Вывести числа от 1 до N без использования цикла
+
+
+void Loop(int N,int i)
+{
+    if (i<=N)
+    {
+        //System.Console.WriteLine(i);
+        Loop(N,i+1);
+    }    
+}
+
+
+Loop(24086,1);
