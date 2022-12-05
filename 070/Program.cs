8f66881dea01805115be4fcf1df3239391fc9c13
Roman Kholev <RKholev@gmail.com>
2022-12-05 15:21:47 +0300

    Добавлено задание 70

--- 070/Program.cs
+++ 070/Program.cs
@@ -1,1 +1,14 @@
 ﻿// 70. Показать натуральные числа от 1 до N, N задано
+
+void Loop(int N,int i)
+{
+    if (i<=N)
+    {
+        System.Console.WriteLine(i);
+        Loop(N,i+1);
+    }    
+}
+
+System.Console.WriteLine("Введите N");
+int n=Convert.ToInt32(System.Console.ReadLine());
+Loop(n,1);
