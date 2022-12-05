95651a770996859ffb0c21d1cae28a2f4695cc59
Roman Kholev <RKholev@gmail.com>
2022-12-05 15:26:54 +0300

    Добавлено задание 71

---
+++ 071/Program.cs
@@ -0,0 +1,13 @@
+﻿//71. Показать натуральные числа от N до 1, N задано
+void Loop(int N,int i)
+{
+    if (i<=N)
+    {
+        System.Console.WriteLine(N);
+        Loop(N-1,i);
+    }    
+}
+
+System.Console.WriteLine("Введите N");
+int n=Convert.ToInt32(System.Console.ReadLine());
+Loop(n,1);
