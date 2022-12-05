f0c98a0d893cb8d026e4bae0db1240df681a627b
Roman Kholev <RKholev@gmail.com>
2022-12-05 17:40:23 +0300

    Добавлено задание 75

---
+++ 075/Program.cs
@@ -0,0 +1,16 @@
+﻿// 75. Написать программу вычисления функции Аккермана - 5*
+
+double Akkerman(double n, double m)
+{  
+     if (n == 0) return m + 1;
+         else 
+            if (n > 0 && m == 0) return Akkerman(n - 1, 1);
+                else
+                   if (n > 0 && m > 0) return Akkerman(n - 1, Akkerman(n, m - 1));
+                         else return 0;
+}
+System.Console.WriteLine("Введите M");
+double m=Convert.ToDouble(System.Console.ReadLine());
+System.Console.WriteLine("Введите N");
+double n=Convert.ToDouble(System.Console.ReadLine());
+System.Console.WriteLine($"Значение функции Аккермана A({m},{n})={Akkerman(m,n)}");
