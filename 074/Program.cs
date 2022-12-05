0c710c8e9e4cab32c64462b2808730bed82adc9b
Roman Kholev <RKholev@gmail.com>
2022-12-05 17:05:40 +0300

    Добавлено задание 74

---
+++ 074/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму цифр числа
+ int SumDigit(int N)
+{
+    if (N==0) return 0;
+     else
+       return SumDigit(N/10)+N%10;
+}
+System.Console.WriteLine("Введите число N");
+int n=Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine(SumDigit(n));
