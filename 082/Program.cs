20c7e61616d1c7be6b6b3e36732c884804a0cfb5
Roman Kholev <RKholev@gmail.com>
2022-12-08 15:54:57 +0300

    Добавлено задание 82

---
+++ 082/Program.cs
@@ -0,0 +1,20 @@
+﻿// 82. C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+int FindeAndCount(string s="пример.пример")
+{
+char [] ca=s.ToCharArray();
+int counter=0;
+for (int i = 0; i < ca.Length; i++)
+{
+if (ca[i]=='.') break;
+  counter++;  
+}
+if (counter==ca.Length) counter=-1;
+return counter;
+}
+
+System.Console.WriteLine("Ведите строку разделенную точкой");
+string s=Console.ReadLine();
+if (FindeAndCount(s)<0) System.Console.WriteLine($"В строке нет точки");
+    else System.Console.WriteLine($"До точки {FindeAndCount(s)} символов");
+
+
