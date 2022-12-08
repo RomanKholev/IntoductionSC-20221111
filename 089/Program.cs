494e3f04d30632c3616289049fd0450ee88be932
Roman Kholev <RKholev@gmail.com>
2022-12-08 17:10:07 +0300

    Добавлено задание 89

---
+++ 089/Program.cs
@@ -0,0 +1,21 @@
+﻿// Подсчитать сумму цифр, встречающихся в строке
+int NumbersSum(string s)
+{
+int sum=0;
+int k=0;
+for(int i=0;i<s.Length;i++)
+
+if (char.IsDigit(s[i]))
+{
+ 
+ sum=sum+s[i]-'0';
+ k++;
+ 
+}
+if (k==0) sum=-1;
+return sum;
+}
+ System.Console.WriteLine("Введите строку"); 
+  string s=Console.ReadLine();
+if (NumbersSum(s)<0) System.Console.WriteLine("В веденной строке нет цифр");
+else System.Console.WriteLine($"Cумма встречающихся цифр = {NumbersSum(s)}");  
