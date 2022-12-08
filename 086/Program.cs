0be2477342ebaa6a8718e8a5b0efc9b3db9360e6
Roman Kholev <RKholev@gmail.com>
2022-12-08 16:55:09 +0300

    Добавлено задание 86

---
+++ 086/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать сколько раз определенный символ (например 'a') встречается в строке.
+// 82. C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+int FindeAndCount(string s, char c)
+{
+int counter=0;
+for (int i = 0; i < s.Length; i++)
+    if (s[i]==c) counter++;
+return counter;
+}
+
+System.Console.WriteLine("Ведите строку");
+string s=Console.ReadLine();
+System.Console.WriteLine("Ведите искомый символ");
+char c=Convert.ToChar(Console.ReadLine());
+System.Console.WriteLine($"Символ {c} встречается в строке {FindeAndCount(s,c)} раз"); 
+
+
